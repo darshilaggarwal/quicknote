365fa05b0f8f0ff5931ef0e58a5aa2fcfd25ca46
darshilaggarwal <darshilaggarwal55@gmail.com>
2025-02-02 14:41:40 +0530

    register UI

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/quickchirp')
 
 const userSchema = mongoose.Schema({
     username : String ,
+    name : String,
     password : String ,
     email : String ,
     age : Number ,
